629bb42fcf4eeeeb9db3abe01a2ff1e462f7f175
Kujiku <ahmedabdallah3996@gmail.com>
2025-10-11 01:09:38 +0300

    refactor: remove build from route regex and update vite config
    - Remove 'build' from route regex pattern to simplify routing
    - Update vite config to use root base path and set assets directory

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/build/',
+  base: '/',
   plugins: [// Docs: https://github.com/posva/unplugin-vue-router
     // ℹ️ This plugin should be placed before vue plugin
     VueRouter({
@@ -96,6 +96,7 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 5000,
+    assetsDir: 'build/assets',
   },
   optimizeDeps: {
     exclude: ['vuetify'],
